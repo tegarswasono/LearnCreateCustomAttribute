6c7f128e11d1982ef6259141b44c88131f9eb7c1
tegarswasono <tegar.s@weefer.co.id>
2022-10-15 16:56:22 +0700

    first commit

---
+++ LearnBasicAuth/LearnBasicAuth/Auth/BasicAuthAttribute.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnBasicAuth.Auth
+{
+    public class BasicAuthMyBroAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var registeredUser = new Dictionary<string, string>()
+            {
+                { "user1", "password1" },
+                { "user2", "password2" },
+                { "user3", "password3" }
+            };
+            if (context.HttpContext.Request.Headers["Authorization"].FirstOrDefault() == null)
+            {
+                context.Result = new UnauthorizedResult();
+            }
+            else
+            {
+                var authToken = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+                if (authToken.Contains("Basic "))
+                {
+                    authToken = authToken.Replace("Basic ", "");
+                }
+                var decodedToken = Encoding.GetEncoding("iso-8859-1").GetString(Convert.FromBase64String(authToken));
+                var separatorIndex = decodedToken.IndexOf(":");
+                var username = decodedToken.Substring(0, separatorIndex);
+                var password = decodedToken.Substring(separatorIndex + 1);
+
+                var isExist = registeredUser.Any(x => x.Key == username && x.Value == password);
+                if (!isExist)
+                {
+                    context.Result = new UnauthorizedResult();
+                }
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ LearnBasicAuth/LearnBasicAuth/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using LearnBasicAuth.Auth;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnBasicAuth.Controllers
+{
+    //ini kita buat attribute sendiri dan kita cek apakah ada passing header authorization gak di sana
+    //sebenarnya ada cara yang lebih bagus, jadi kita pakai attribute bawaan aspnet core yaitu [Authorize]
+    //cara setup nya gimana, baca sendiri ya di internet, yg jelas biasa nya kamu di minta extends ke class / interface tertentu 
+    //lalu daftarkan class yang baru kamu buat ke startup.cs, app.UseAuthentication(); => code ini jelas harus di tambahkan 
+
+    [ApiController]
+    [Route("api/[controller]")]
+    [BasicAuthMyBro]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public ActionResult<List<ProductModel>> Index()
+        {
+            var tmp = new List<ProductModel>()
+            {
+                new ProductModel(){ Name = "Baju Anak", Stok = 5, Price=50000 },
+                new ProductModel(){ Name = "Baju Pria", Stok = 10, Price=20000 },
+                new ProductModel(){ Name = "Baju Wanita", Stok = 6, Price=80000 },
+                new ProductModel(){ Name = "Baju Sekolah", Stok = 20, Price=40000 }
+            };
+            return Ok(tmp);
+        }
+    }
+    public class ProductModel
+    {
+        public string Name { set; get; }
+        public long Stok { set; get; }
+        public double Price { set; get; }
+    }
+}
